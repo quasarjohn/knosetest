0af1cfd2fa196b0ae5e8f7a1875767c5446f232a
thefigman <gilbert.quavi@gmail.com>
2023-03-04 20:34:36 +1000

    Update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const printKnoseSlogan = require("./printKnoseSlogan");
 const printTriangle = require("./printTriangle");
-const sorting = require("./sorting");
+const testSort = require("./testSort");
 
 printKnoseSlogan(100);
 printTriangle(20);
-sorting();
+testSort();
